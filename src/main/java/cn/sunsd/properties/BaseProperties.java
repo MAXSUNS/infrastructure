14b6da8233a2df3a083c657fccdfdf89b75dcb73
MAXSUNS <sundai.cool@hotmail.com>
2018-01-09 20:07:18 +0800

    build base

---
+++ src/main/java/cn/sunsd/properties/BaseProperties.java
@@ -0,0 +1,7 @@
+package cn.sunsd.properties;
+
+/**
+ * Created by sund on 2018/1/9.
+ */
+public class BaseProperties {
+}
---
+++ src/main/java/cn/sunsd/service/baseService.java
@@ -0,0 +1,7 @@
+package cn.sunsd.service;
+
+/**
+ * Created by sund on 2018/1/9.
+ */
+public class baseService {
+}
