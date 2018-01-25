92699a315eef2e85f81e5ba23d3aa5b337e81306
MAXSUNS <sundai.cool@hotmail.com>
2018-01-25 16:57:40 +0800

    build base

---
+++ src/main/java/cn/sunsd/aware/TestApplicationContext.java
@@ -0,0 +1,15 @@
+package cn.sunsd.aware;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Created by sund on 2018/1/25.
+ */
+public class TestApplicationContext implements ApplicationContextAware {
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
+}
